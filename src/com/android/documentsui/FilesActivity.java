821727a0fd0fa608a3046db8dad6ea588e16828f
Ben Lin <linben@google.com>
2016-07-08 10:49:53 -0700

    Follow up changes from ag/1190582.
    Change-Id: I40121fc3d36e430c402da2835027283eb4b11071

--- src/com/android/documentsui/FilesActivity.java
+++ src/com/android/documentsui/FilesActivity.java
@@ -90,7 +90,7 @@ public class FilesActivity extends BaseActivity {
             }
         };
 
-        RootsFragment.show(getFragmentManager(), null);
+        RootsFragment.show(getFragmentManager(), this::openRootSettings);
 
         final Intent intent = getIntent();
         final Uri uri = intent.getData();
@@ -240,7 +240,6 @@ public class FilesActivity extends BaseActivity {
         return true;
     }
 
-    @Override
     void openRootSettings(RootInfo root) {
         Metrics.logUserAction(this, Metrics.USER_ACTION_SETTINGS);
         final Intent intent = new Intent(DocumentsContract.ACTION_DOCUMENT_ROOT_SETTINGS);
