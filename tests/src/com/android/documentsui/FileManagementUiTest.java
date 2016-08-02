66fb96a0d6908dc5d0e3017dc5c2680d04788d15
Ben Lin <linben@google.com>
2016-08-01 17:58:02 -0700

    Partial revert of ag/1239898.
    Change-Id: I18c1d7289e40f2df36ff1323c485d0195f997606

--- tests/src/com/android/documentsui/FileManagementUiTest.java
+++ tests/src/com/android/documentsui/FileManagementUiTest.java
@@ -67,6 +67,17 @@ public class FileManagementUiTest extends ActivityTest<FilesActivity> {
         bots.directory.waitForDocument("Kung Fu Panda");
     }
 
+    public void testDeleteDocument() throws Exception {
+        bots.directory.clickDocument("file1.png");
+        device.waitForIdle();
+        bots.main.clickToolbarItem(R.id.menu_delete);
+
+        bots.main.clickDialogOkButton();
+        device.waitForIdle();
+
+        bots.directory.assertDocumentsAbsent("file1.png");
+    }
+
     public void testKeyboard_CutDocument() throws Exception {
         bots.directory.clickDocument("file1.png");
         device.waitForIdle();
@@ -100,7 +111,7 @@ public class FileManagementUiTest extends ActivityTest<FilesActivity> {
         bots.directory.waitForDocument("file1.png");
     }
 
-    public void testDeleteDocument_Cancel_ThenOK() throws Exception {
+    public void testDeleteDocument_Cancel() throws Exception {
         bots.directory.clickDocument("file1.png");
         device.waitForIdle();
         bots.main.clickToolbarItem(R.id.menu_delete);
@@ -108,13 +119,5 @@ public class FileManagementUiTest extends ActivityTest<FilesActivity> {
         bots.main.clickDialogCancelButton();
 
         bots.directory.waitForDocument("file1.png");
-
-        device.waitForIdle();
-        bots.main.clickToolbarItem(R.id.menu_delete);
-
-        bots.main.clickDialogOkButton();
-        device.waitForIdle();
-
-        bots.directory.assertDocumentsAbsent("file1.png");
     }
 }
