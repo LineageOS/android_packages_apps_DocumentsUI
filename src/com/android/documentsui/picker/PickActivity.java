52fe423907ac089fff5c56d84261ebd0c0506519
Tomasz Mikolajewski <mtomasz@google.com>
2016-11-25 14:23:39 +0900

    Do not open archives inline in picker.
    Otherwise it's impossible to select archives.
    
    Test: Tested with Gmail.
    Bug: 32486431
    Change-Id: I894c6d15af6c8ab439ab4234d3deb01914c22b31
    Signed-off-by: Tomasz Mikolajewski <mtomasz@google.com>
    (cherry picked from commit 912e3652f068465e991d04db0f808eccf9a30640)

--- src/com/android/documentsui/picker/PickActivity.java
+++ src/com/android/documentsui/picker/PickActivity.java
@@ -338,7 +338,9 @@ public class PickActivity extends BaseActivity implements ActionHandler.Addons {
     @Override
     public void onDocumentPicked(DocumentInfo doc) {
         final FragmentManager fm = getFragmentManager();
-        if (doc.isContainer()) {
+        // Do not inline-open archives, as otherwise it would be impossible to pick
+        // archive files. Note, that picking files inside archives is not supported.
+        if (doc.isDirectory()) {
             mInjector.actions.openContainerDocument(doc);
         } else if (mState.action == ACTION_OPEN || mState.action == ACTION_GET_CONTENT) {
             // Explicit file picked, return
