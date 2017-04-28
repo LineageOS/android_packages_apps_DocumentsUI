a14c17336595dbbd489839180198280b01b7f3aa
Ben Lin <linben@google.com>
2017-04-27 17:05:24 -0700

    DO NOT MERGE Changing configuration so we don't build with an odex file in buildbots.
    Change-Id: I293731f5eedf3f7986a7c72b520f9223c92a4470

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
 
+LOCAL_DEX_PREOPT := false
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
