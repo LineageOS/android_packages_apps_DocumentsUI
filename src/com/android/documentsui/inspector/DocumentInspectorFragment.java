ef5a0a55b04f21a6b75b7640101c6808da0d8f09
Austin Kolander <greensurfer@google.com>
2017-06-09 11:34:23 -0700

    Encapsulate fragment in activity
    Change-Id: I8daccb95fb49a99d933f5838006e6841c3365296

--- src/com/android/documentsui/inspector/DocumentInspectorFragment.java
+++ src/com/android/documentsui/inspector/DocumentInspectorFragment.java
@@ -16,6 +16,7 @@
 package com.android.documentsui.inspector;
 
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,9 +25,24 @@ import com.android.documentsui.R;
 
 public class DocumentInspectorFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.document_inspector_fragment, container, false);
-    }
+  private static final String DOC_URI_ARG = "docUri";
+  private Uri docUri;
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+
+      Bundle args = getArguments();
+      docUri = (Uri) args.get(DOC_URI_ARG);
+
+      return inflater.inflate(R.layout.document_inspector_fragment, container, false);
+  }
+
+  public static DocumentInspectorFragment newInstance(Uri uri) {
+    Bundle args = new Bundle();
+    args.putParcelable(DOC_URI_ARG, uri);
+    DocumentInspectorFragment fragment = new DocumentInspectorFragment();
+    fragment.setArguments(args);
+    return fragment;
+  }
 }
