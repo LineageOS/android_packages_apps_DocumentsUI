997e95bd2ec884cfdf708083faeea066301f8f90
Bill Lin <lbill@google.com>
2018-09-12 22:41:28 +0800

    Implement Dark Theme UI TestCase
    1) Implement Dark/Light theme test base on Resource.theme mechanism
    2) Refine stylable android attr, do not override format attr
    
    Bug: 115315612
    Test: atest com.android.documentsui.ui
    Change-Id: I087420b286d6bec0e7951f1acdeea868bc2afe23

---
+++ tests/unit/com/android/documentsui/ui/DarkThemeUiTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.documentsui.ui;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.android.documentsui.R;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * This class test default Dark Theme (Night Mode Disable)
+ * Verify ActionBar background, Window background, and GridItem background to meet Light style
+ */
+@SmallTest
+@RunWith(AndroidJUnit4.class)
+public class DarkThemeUiTest extends ThemeUiTestBase {
+    Context mTestContext;
+    int mExpectedDarkGreyColor, mExpectedGreyColor;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        mTestContext = InstrumentationRegistry.getContext();
+        mTheme = getThemeByUiMode(mTargetContext, Configuration.UI_MODE_NIGHT_YES);
+        mExpectedDarkGreyColor = mTestContext.getResources().getColor(
+                com.android.documentsui.tests.R.color.expected_g_dark_grey);
+        mExpectedGreyColor = mTestContext.getResources().getColor(
+                com.android.documentsui.tests.R.color.expected_g_grey);
+    }
+
+    @Test
+    public void themeNightModeEnable_actionBarColorShouldBeDark() {
+
+        assertTheme(R.styleable.ActionBarView, R.styleable.ActionBarView_android_colorPrimary,
+                mExpectedDarkGreyColor);
+    }
+
+    @Test
+    public void themeNightModeEnable_gridItemBackgroundColorShouldBeDark() {
+        assertTheme(R.styleable.GridItem, R.styleable.GridItem_gridItemColor, mExpectedGreyColor);
+    }
+
+    @Test
+    public void themeNightModeEnable_windowLightNavigationBarShouldBeFalse() {
+        assertTheme(R.styleable.SystemWindow,
+                R.styleable.SystemWindow_android_windowLightNavigationBar, false);
+    }
+
+    @Test
+    public void themeNightModeEnable_windowLightStatusBarShouldBeFalse() {
+        assertTheme(R.styleable.SystemWindow,
+                R.styleable.SystemWindow_android_windowLightNavigationBar, false);
+    }
+
+    @Test
+    public void themeNightModeEnable_navigationBarColorShouldBeDark() {
+        assertTheme(R.styleable.SystemWindow, R.styleable.SystemWindow_android_navigationBarColor,
+                mExpectedDarkGreyColor);
+    }
+
+    @Test
+    public void themeNightModeEnable_windowBackgroundColorShouldBeDark() {
+        assertTheme(R.styleable.SystemWindow, R.styleable.SystemWindow_android_windowBackground,
+                mExpectedDarkGreyColor);
+    }
+
+    @Test
+    public void themeNightModeEnable_statusBarColorShouldBeDark() {
+        assertTheme(R.styleable.SystemWindow, R.styleable.SystemWindow_android_statusBarColor,
+                mExpectedDarkGreyColor);
+    }
+}
