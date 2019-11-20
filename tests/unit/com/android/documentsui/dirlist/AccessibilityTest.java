728104043bb49a5e0102430ce97519f5820c9308
Tony Huang <tonyychuang@google.com>
2019-11-20 16:12:24 +0800

    Revert "Fix broken a11y function"
    This reverts commit f6bf0054cf471616bcf2dc715cfeeca411686a70.
    Revert this because previous code can work in current build.
    
    Bug: 144819035
    Test: atest DocumentsUIGoogleTests
    Change-Id: I8d482ab495ce5c1d6a1302d09f79fc155e999de5

--- tests/unit/com/android/documentsui/dirlist/AccessibilityTest.java
+++ tests/unit/com/android/documentsui/dirlist/AccessibilityTest.java
@@ -16,54 +16,84 @@
 
 package com.android.documentsui.dirlist;
 
+import android.database.Cursor;
 import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
 import android.view.View;
-import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.Space;
 
-import androidx.test.filters.SmallTest;
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.documentsui.testing.TestRecyclerView;
 import com.android.documentsui.testing.Views;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SmallTest
 public class AccessibilityTest extends AndroidTestCase {
 
+    private static final List<String> ITEMS = TestData.create(10);
+
+    private TestRecyclerView mView;
     private AccessibilityEventRouter mAccessibilityDelegate;
     private boolean mClickCallbackCalled = false;
     private boolean mLongClickCallbackCalled = false;
 
     @Override
     public void setUp() throws Exception {
-        mAccessibilityDelegate = new AccessibilityEventRouter((View v) -> {
+        mView = TestRecyclerView.create(ITEMS);
+        mAccessibilityDelegate = new AccessibilityEventRouter(mView, (View v) -> {
             mClickCallbackCalled = true;
             return true;
         }, (View v) -> {
             mLongClickCallbackCalled = true;
             return true;
         });
+        mView.setAccessibilityDelegateCompat(mAccessibilityDelegate);
     }
 
     public void test_announceSelected() throws Exception {
         View item = Views.createTestView(true);
-        AccessibilityNodeInfo info = AccessibilityNodeInfo.obtain();
-        mAccessibilityDelegate.onInitializeAccessibilityNodeInfo(item, info);
+        AccessibilityNodeInfoCompat info = AccessibilityNodeInfoCompat.obtain();
+        mAccessibilityDelegate.getItemDelegate().onInitializeAccessibilityNodeInfo(item, info);
         assertTrue(info.isSelected());
+    }
+
+    public void testNullItemDetails_NoActionClick() throws Exception {
+        View item = Views.createTestView(true);
+        AccessibilityNodeInfoCompat info = AccessibilityNodeInfoCompat.obtain();
+
+        List<RecyclerView.ViewHolder> holders = new ArrayList<>();
+        holders.add(new MessageHolder(mView.getContext(), new Space(mView.getContext())) {
+            @Override
+            public void bind(Cursor cursor, String modelId) {
+
+            }
+        });
+
+        mView.setHolders(holders);
+
+        mAccessibilityDelegate.getItemDelegate().onInitializeAccessibilityNodeInfo(item, info);
+        assertFalse(info.isClickable());
     }
 
     public void test_routesAccessibilityClicks() throws Exception {
         View item = Views.createTestView(true);
-        AccessibilityNodeInfo info = AccessibilityNodeInfo.obtain();
-        mAccessibilityDelegate.onInitializeAccessibilityNodeInfo(item, info);
-        mAccessibilityDelegate.performAccessibilityAction(
-                item, AccessibilityNodeInfo.ACTION_CLICK, null);
+        AccessibilityNodeInfoCompat info = AccessibilityNodeInfoCompat.obtain();
+        mAccessibilityDelegate.getItemDelegate().onInitializeAccessibilityNodeInfo(item, info);
+        mAccessibilityDelegate.getItemDelegate().performAccessibilityAction(
+                item, AccessibilityNodeInfoCompat.ACTION_CLICK, null);
         assertTrue(mClickCallbackCalled);
     }
 
     public void test_routesAccessibilityLongClicks() throws Exception {
         View item = Views.createTestView(true);
-        AccessibilityNodeInfo info = AccessibilityNodeInfo.obtain();
-        mAccessibilityDelegate.onInitializeAccessibilityNodeInfo(item, info);
-        mAccessibilityDelegate.performAccessibilityAction(
-                item, AccessibilityNodeInfo.ACTION_LONG_CLICK, null);
+        AccessibilityNodeInfoCompat info = AccessibilityNodeInfoCompat.obtain();
+        mAccessibilityDelegate.getItemDelegate().onInitializeAccessibilityNodeInfo(item, info);
+        mAccessibilityDelegate.getItemDelegate().performAccessibilityAction(
+                item, AccessibilityNodeInfoCompat.ACTION_LONG_CLICK, null);
         assertTrue(mLongClickCallbackCalled);
     }
 }
