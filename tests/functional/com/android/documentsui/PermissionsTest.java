695d9103918bd2b24bfafa257664fe0a0096c401
Kelvin Kwan <kelvinkwan@google.com>
2020-01-28 20:25:08 +0000

    Test DocsUI should have permission on R or above only
    Test: atest DocumentsUIGoogleTests:com.android.documentsui.PermissionsTest
    Bug: 148198996
    Change-Id: I5501b999eb8f229c327dec6c9282fa7b5c3a4f37

--- tests/functional/com/android/documentsui/PermissionsTest.java
+++ tests/functional/com/android/documentsui/PermissionsTest.java
@@ -18,6 +18,7 @@ package com.android.documentsui;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 
 import com.android.documentsui.files.FilesActivity;
 
@@ -37,8 +38,17 @@ public class PermissionsTest extends ActivityTest<FilesActivity> {
     }
 
     @Test
-    public void testPermissionGranted_interactAcrossUsers() {
-        assertEquals(PackageManager.PERMISSION_GRANTED,
-                context.checkSelfPermission(Manifest.permission.INTERACT_ACROSS_USERS));
+    public void testPermissionGranted_interactAcrossUsersOnR() {
+        if (isAtLeastR()) {
+            assertEquals(PackageManager.PERMISSION_GRANTED,
+                    context.checkSelfPermission(Manifest.permission.INTERACT_ACROSS_USERS));
+        }
+    }
+
+    private static boolean isAtLeastR() {
+        return (Build.VERSION.CODENAME.equals("REL") && Build.VERSION.SDK_INT >= 30)
+                || (Build.VERSION.CODENAME.length() == 1
+                && Build.VERSION.CODENAME.charAt(0) >= 'R'
+                && Build.VERSION.CODENAME.charAt(0) <= 'Z');
     }
 }
