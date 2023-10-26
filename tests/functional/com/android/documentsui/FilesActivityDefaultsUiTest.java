fb49a932bf8d1908e1e7cf89062375d62939a689
Colin Cross <ccross@android.com>
2023-10-26 14:57:57 +0000

    Revert "Build DocumentsUI sources once"
    Revert submission 25032702-DocumentsUI-lib
    
    Reason for revert: broke document browsing
    
    Reverted changes: /q/submissionid:25032702-DocumentsUI-lib
    
    Bug: 307873144
    Bug: 307882692
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:0d1e2f46a5dde64df8506691afe89efda697a60c)
    Merged-In: I5042f5f430f515cfe648156f21f7570e43aabbe6
    Change-Id: I5042f5f430f515cfe648156f21f7570e43aabbe6

--- tests/functional/com/android/documentsui/FilesActivityDefaultsUiTest.java
+++ tests/functional/com/android/documentsui/FilesActivityDefaultsUiTest.java
@@ -21,6 +21,7 @@ import static com.android.documentsui.StubProvider.ROOT_1_ID;
 
 import android.os.RemoteException;
 
+import androidx.recyclerview.R;
 import androidx.test.filters.LargeTest;
 
 import com.android.documentsui.base.RootInfo;
