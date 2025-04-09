9fa636e25d5c5886278ed31662a58978c37f615a
Ben Reich <benreich@google.com>
2025-04-09 11:02:01 +1000

    Trim the application name to make it safe for presentation
    The application name is presented in the ConfirmFragment and as such we
    don't want to allow for any length. This follows a similar approach to
    PackageManager using the TextUtils.makeSafeForPresentation with a total
    available character length of 500.
    
    This removes the unused getCallingAppName from the DirectoryFragment as
    it was causing false positives from DirectoryFragment to avoid false
    positives when trying to find who calls the Shared function.
    
    On top of this, add some quotation marks around the app name to avoid
    the app name being a contination of the existing text in the dialog,
    e.g. 'This will let app name access current and future content storage
    in Alarms' will now be 'This will let "app name" access current and
    future content storage'.
    
    Bug: 397216537
    Test: atest com.android.documentsui.picker.ApplicationNameTest
    Flag: EXEMPT bug fix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c8ef2db3bb4645704384226976e59583b9e8d3d3)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:9ed806f492cb7a8e2360c5c09caac87cb5d0f823)
    Merged-In: Iad0d03de09b1e4ad953bd6bd46a619cfcc56d384
    Change-Id: Iad0d03de09b1e4ad953bd6bd46a619cfcc56d384

--- src/com/android/documentsui/dirlist/DirectoryFragment.java
+++ src/com/android/documentsui/dirlist/DirectoryFragment.java
@@ -1560,10 +1560,5 @@ public class DirectoryFragment extends Fragment implements SwipeRefreshLayout.On
         public ActionHandler getActionHandler() {
             return mActions;
         }
-
-        @Override
-        public String getCallingAppName() {
-            return Shared.getCallingAppName(mActivity);
-        }
     }
 }
